cb9484c84c67eaeef070d63c56d814537a1156e9
Zhenya Dikaya <zhenyadikaya94@gmail.com>
2025-07-03 11:53:28 +0300

    feat: feat data supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("supabaseUrl is required");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
